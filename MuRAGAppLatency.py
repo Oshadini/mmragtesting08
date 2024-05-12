d32515fc3ec9cf3ec6ebbe56c45007fef35cbac1
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2024-05-13 01:31:53 +0530

    Update MuRAGAppLatency.py

--- MuRAGAppLatency.py
+++ MuRAGAppLatency.py
@@ -290,7 +290,7 @@ if uploaded_file is not None:
     st.write(f"{bullet_point} \t\tImage summaries generation completed") 
     st.write(f"{bullet_point} Summary generation process completed")  
         
-    
+    @st.cache_data()
     def create_multi_vector_retriever(
       _vectorstore, text_summaries, texts, table_summaries, tables, image_summaries, images
     ):
